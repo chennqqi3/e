485a02cf0328f250a62f480208c776fc7a978292
alanzhuang <522143443@qq.com>
2017-09-08 12:38:33 +0800

    Delete weixin_2.py

--- ok/weixin_2.py
+++
@@ -1,6 +0,0 @@
-import requests
-headers={
-   'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'
-}
-url='https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIwNzM2MjA4OA==&scene=124&#wechat_redirect'
-print requests.get(url,verify=False).text
